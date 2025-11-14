4f35da058cfb8b61b72d1191e67b219cd10968ea
Borys <lolo.szczygiel@outlook.com>
2025-11-14 20:16:03 +0100

    atomslist

--- app/displays/crafting.py
+++ app/displays/crafting.py
@@ -1,3 +1,5 @@
+from email.mime import image
+
 import pyray as rl
 from app.displays.base import BaseDisplay
 from app.cameras import twodcamera
@@ -27,17 +29,19 @@ class Crafting_Menu(BaseDisplay):
         self.objects = []
         # a=GameObject(self,assets.images["Jeff"],0,0,10,10)
         # t1=TextObject(self,'aaa',0,0,100,rl.WHITE)
-        self.inventory=Inventory({"oxygen":2,"hydrogen":2},self,0,0,50,200)
-        self.oxygen1=Atom(self,assets.images["Oxygen_Standby"] ,1000,100,100,100,"O",8,40)
+        self.inventory=Inventory({"oxygen":2,"hydrogen":2,"zinc":3,"sodium":0,"krypton":10,"barium":11},self,0,0,120,200)
+        # self.oxygen1=Atom(self,assets.images["Oxygen_Standby"] ,1000,100,100,100,"O",8,40)
+        self.atom_bar=Atom_Bar(self,500,0,1000,100)
+        self.table=Table(self,500,500,500,500)
     def render(self):
-        print(self.square_pos)
+        # print(self.square_pos)
         super().render()
 
 
         rl.draw_fps(10, 10)
         # rl.draw_rectangle(int(self.square_pos[0]), int(self.square_pos[1]), 20, 20, rl.RED)
         for obj in self.objects:
-            print('aa')
+            # print('aa')
             obj.draw()
 
 
@@ -57,19 +61,25 @@ class Crafting_Menu(BaseDisplay):
         if rl.is_key_pressed(rl.KeyboardKey.KEY_C) or rl.is_gamepad_button_pressed(self.game.gamepad_id, rl.GamepadButton.GAMEPAD_BUTTON_RIGHT_FACE_UP):
             self.game.current_display = self.game.twodgame
             self.game.crafting = False
+        if rl.is_mouse_button_pressed(0):
+            self.mouse=rl.get_mouse_position()
+            print(self.mouse.x,self.mouse.y)
 
-        # if not self.game.gamepad_enabled:
-        #     if rl.is_key_down(rl.KeyboardKey.KEY_W):
-        #         self.square_pos[1] -= self.speed * self.delta_time
-        #     if rl.is_key_down(rl.KeyboardKey.KEY_S):
-        #         self.square_pos[1] += self.speed * self.delta_time
-        #     if rl.is_key_down(rl.KeyboardKey.KEY_A):
-        #         self.square_pos[0] -= self.speed * self.delta_time
-        #     if rl.is_key_down(rl.KeyboardKey.KEY_D):
-        #         self.square_pos[0] += self.speed * self.delta_time
-        # else:
-        #     self.square_pos[0] += self.game.left_joystick_x * self.speed * self.delta_time
-        #     self.square_pos[1] += self.game.left_joystick_y * self.speed * self.delta_time
+            for x in self.atom_bar.atom_images.keys():
+                obj=self.atom_bar.atom_images[x]
+                if self.collision(obj,self.mouse):
+                    if self.inventory.inv[x]>=1:
+                        self.inventory.inv[x]-=1
+
+    def collision(self,obj1,mouse):
+        if (obj1.x<mouse.x<obj1.x+obj1.w) :
+            if (obj1.y<mouse.y<obj1.y+obj1.h):
+                return True
+        return False
+
+
+    def get_inv(self):
+        return self.inventory.inv
 class GameObject():
     def __init__(self,display,image,x,y,w,h,scale):
         self.display = display
@@ -86,11 +96,15 @@ class GameObject():
         self.h = h
         self.square_pos = [self.x, self.y]
         self.display.objects.append(self)
+
     def draw(self):
-        print('bb')
+        # print('bb')
+
 
         rl.draw_texture_ex(self.image, rl.Vector2(float(self.square_pos[0]), float(self.square_pos[1])), 0.0,
                            self.w/self.scale, rl.WHITE)
+    def delete(self):
+        self.display.objects.remove(self)
 class TextObject():
     def __init__(self,display,text,x,y,w,color):
         self.display = display
@@ -103,8 +117,10 @@ class TextObject():
         self.display.objects.append(self)
 
     def draw(self):
-        print('bb')
+        # print('bb')
         rl.draw_text(self.text,self.x,self.y,self.w,self.color,)
+    def delete(self):
+        self.display.objects.remove(self)
 class Rect():
     def __init__(self,display,x,y,w,h):
         self.display = display
@@ -114,8 +130,10 @@ class Rect():
         self.h = h
         self.display.objects.append(self)
     def draw(self):
-        print('bb')
+        # print('bb')
         rl.draw_rectangle_lines(self.x,self.y,self.w,self.h,rl.WHITE,)
+    def delete(self):
+        self.display.objects.remove(self)
 
 
 class Inventory():
@@ -128,6 +146,9 @@ class Inventory():
         self.inv={}
         self.display.objects.append(self)
         self.inv=inv
+        self.atom_images={}
+
+
     def add_element(self,atom,amount):
         if atom in self.inv:
             self.inv[atom] += amount
@@ -139,6 +160,7 @@ class Inventory():
         for atom in self.inv:
             i+=1
             rl.draw_text(f"{atom}: {self.inv[atom]}",self.x,self.y+self.w*i,self.w,rl.WHITE)
+
 class Atom():
     def __init__(self,display,image,x,y,w,h,name,mass,font_w):
         self.display = display
@@ -154,5 +176,32 @@ class Atom():
         self.name_text=TextObject(display,self.name,x,y-3,font_w,rl.WHITE)
         self.mass_text=TextObject(display,str(self.mass),x+self.w-self.font_w//2,y+self.h-self.font_w+9,font_w,rl.WHITE)
         self.rect=Rect(display,x,y,w,h)
+        self.hitboxes=rl.Rectangle(x,y,w,h)
 
+
+class Atom_Bar():
+    def __init__(self,display,x,y,w,h):
+        self.display = display
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.inv=self.display.get_inv()
+        self.atom_images={}
+        atom_properites={"hydrogen":['H',1,assets.images["Hydrogen_Standby"]],"helium":['He',2,assets.images["Helium_Standby"]],"oxygen":['O',8,assets.images["Oxygen_Standby"]],"sodium":['Na',11,assets.images["Sodium_Standby"]],"iron":['Fe',26,assets.images["Iron_Standby"]],"zinc":['Zn',30,assets.images["Zinc_Standby"]],"barium":['Ba',56,assets.images["Barium_Standby"]],"krypton":['Kr',36,assets.images["Krypton_Standby"]]}
+        i=0
+        for a in self.inv.keys():
+            print(a)
+            self.atom_images[a]=Atom(self.display, atom_properites[a][2], x + self.w-i*100, y, 100, 100, atom_properites[a][0], atom_properites[a][1], 40)
+            i += 1
+class Table():
+    def __init__(self,display,x,y,w,h):
+        self.display = display
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.protons=0
+        self.atoms=[]
+        self.rect=Rect(display,x,y,w,h)
 
