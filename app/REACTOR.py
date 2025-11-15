118539eff3ca155ea9640c33ff0c105bf34b08fa
Borys <lolo.szczygiel@outlook.com>
2025-11-15 05:51:00 +0100

    map

--- app/REACTOR.py
+++ app/REACTOR.py
@@ -4,6 +4,6 @@ class Reactor(sprite.Sprite):
     def __init__(self, display, scaleXframewidth=60):
         self.img = assets.images["Elektrownia"]
         super().__init__(display, scaleXframewidth=scaleXframewidth)
-        self.x = 200
-        self.y = 189
+        self.x = 220
+        self.y = 200
 
